c3d5abbe541184ab30e6fca937a195d8df6c5933
Houcine <106538937+Hcn-88@users.noreply.github.com>
2023-01-03 16:27:21 -0300

    first commit

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,3 +1,4 @@
+import { Product } from './Product';
 import { Component } from '@angular/core';
 
 @Component({
@@ -7,4 +8,27 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'crud';
+
+  product?: Product
+  products: Product[] = []
+
+  onSubmit(product: Product){
+    this.products.push(product)
+  }
+
+  onSelect(id: number){
+    this.product = this.products[id];
+    this.product!.id = id;
+  }
+
+  onDelete(id: number) {
+    this.products = this.products.filter((item, indice) => indice != id)
+  }
+
+  onEdit(product: Product) {
+    let id = product.id!;
+    this.products[id] = product;
+    return this.products;
+  }
+
 }
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,16 +1,23 @@
 import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
+import { FormsModule, ReactiveFormsModule } from '@angular/forms';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { TabelaComponent } from './components/tabela/tabela.component';
+import { FormComponent } from './components/form/form.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    TabelaComponent,
+    FormComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    FormsModule,
+    ReactiveFormsModule
   ],
   providers: [],
   bootstrap: [AppComponent]
